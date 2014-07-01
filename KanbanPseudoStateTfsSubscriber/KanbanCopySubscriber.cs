664f0ed62c5e14ddc36a0b77199986ddc3270620
shawnweisfeld <shawn@shawnweisfeld.com>
2014-06-30 18:30:36 -0700

    init
    init

---
+++ KanbanPseudoStateTfsSubscriber/KanbanCopySubscriber.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.TeamFoundation.Common;
+using Microsoft.TeamFoundation.Framework.Server;
+using Microsoft.TeamFoundation.WorkItemTracking.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanPseudoStateTfsSubscriber
+{
+    public class KanbanCopySubscriber : ISubscriber
+    {
+        public string Name
+        {
+            get { return this.GetType().FullName; }
+        }
+
+        public SubscriberPriority Priority
+        {
+            get { return SubscriberPriority.Low; }
+        }
+
+
+        public Type[] SubscribedTypes()
+        {
+            return new[]
+                       {
+                           typeof(WorkItemChangedEvent)
+                       };
+        }
+
+        public EventNotificationStatus ProcessEvent(TeamFoundationRequestContext requestContext, 
+            NotificationType notificationType, object notificationEventArgs, out int statusCode, out string statusMessage, 
+            out Microsoft.TeamFoundation.Common.ExceptionPropertyCollection properties)
+        {
+            statusCode = 0;
+            properties = null;
+            statusMessage = string.Empty;
+
+            try
+            {
+                var workItemEvent = notificationEventArgs as WorkItemChangedEvent;
+
+                if (notificationType == NotificationType.Notification
+                    && workItemEvent != null
+                    && workItemEvent.ChangedFields != null)
+                {
+                    int id = GetWorkItemId(workItemEvent);
+
+                    if (id != 0)
+                    {
+                        var kanbanColumnName = GetCurrentKanbanColumn(requestContext, id);
+
+                        if (!string.IsNullOrEmpty(kanbanColumnName))
+                        {
+                            TfsClientHelper.UpdateKanbanColumn(id, kanbanColumnName, requestContext);
+                        }
+
+                        return EventNotificationStatus.ActionPermitted;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var log = new EventLog();
+                log.Source = "TFS Services";
+                if (!EventLog.SourceExists(log.Source))
+                {
+                    EventLog.CreateEventSource(log.Source, "Application");
+                }
+
+                string strMessage = string.Format("{0} - {1}\r\n{2}", this.GetType().FullName, ex.Message, ex.StackTrace);
+                log.WriteEntry(strMessage, EventLogEntryType.Error);
+
+                return EventNotificationStatus.ActionDenied;
+            }
+
+            return EventNotificationStatus.ActionApproved;
+        }
+
+        private string GetCurrentKanbanColumn(TeamFoundationRequestContext requestContext, int id)
+        {
+            var extService = new WorkItemTypeExtensionService();
+            var workItemService = new WorkItemService();
+            var svrWI = workItemService.GetWorkItem(requestContext, id);
+
+            foreach (var wef in extService.GetExtensions(requestContext, svrWI.WorkItemTypeExtensionIds))
+            {
+                var foo = wef.Fields.Select(x => new { x.Field.FieldId, x.LocalName, x.LocalReferenceName });
+
+                foreach (var field in wef.Fields)
+                {
+                    if (field.LocalReferenceName == "Kanban.Column" && field.LocalName == "Kanban Column")
+                    {
+                        return svrWI.LatestData[field.Field.FieldId].ToString();
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private int GetWorkItemId(WorkItemChangedEvent workItemEvent)
+        {
+            foreach (IntegerField item in workItemEvent.CoreFields.IntegerFields)
+            {
+                if (string.Compare(item.Name, "ID", true) == 0)
+                {
+                    return item.NewValue;
+                }
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ KanbanPseudoStateTfsSubscriber/TfsClientHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Server;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanPseudoStateTfsSubscriber
+{
+    public static class TfsClientHelper
+    {
+        public static void UpdateKanbanColumn(int id, string newKanbanColumn, TeamFoundationRequestContext requestContext)
+        {
+            Uri uri = GetTFSUri(requestContext);
+            using (var tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(uri))
+            {
+                var wis = tfs.GetService<WorkItemStore>();
+                WorkItem wiCurrent = wis.GetWorkItem(id);
+
+                var wiTypes = new List<string>()
+                {
+                    "Product Backlog Item",
+                    "User Story"
+                };
+
+                var currentWIType = wiCurrent.Fields["Work Item Type"].Value.ToString();
+
+                if (wiCurrent.Fields.Contains("KanBan State")
+                    && wiTypes.Any(x => x.Equals(currentWIType, StringComparison.InvariantCultureIgnoreCase))
+                    && wiCurrent.Fields["KanBan State"].Value.ToString() != newKanbanColumn)
+                {
+                    wiCurrent.Fields["KanBan State"].Value = newKanbanColumn;
+                    wiCurrent.Save();
+                    wiCurrent.Close();
+                }
+
+            }
+        }
+
+        public static Uri GetTFSUri(TeamFoundationRequestContext requestContext)
+        {
+            return new Uri(requestContext.GetService<TeamFoundationLocationService>().GetServerAccessMapping(requestContext).AccessPoint.Replace("localhost", Environment.MachineName) + "/" + requestContext.ServiceHost.Name);
+        }
+
+    }
+}
